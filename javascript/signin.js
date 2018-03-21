1d4fe442c79e821e66562b120c44c5e956855acb
Tim Chu <chutianxin@gmail.com>
2018-03-20 22:40:50 -0700

    add mock sign in of two demo accounts

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,21 +1,30 @@
 var Web3 = require('web3');
 var web3 = new Web3();
+var user = 0;
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:9545"));
 function watchBalance() {
-    var coinbase = web3.eth.coinbase;
-    var originalBalance = web3.eth.getBalance(coinbase).toNumber();
-    document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
-    document.getElementById('original').innerText = ' original balance: ' + originalBalance + '    watching...';
-    web3.eth.filter('latest').watch(function () {
-        var currentBalance = web3.eth.getBalance(coinbase).toNumber();
-        document.getElementById("current").innerText = 'current: ' + currentBalance;
-        document.getElementById("diff").innerText = 'diff:    ' + (currentBalance - originalBalance);
+    //var coinbase = web3.eth.coinbase;
+    var coinbase;
+    web3.eth.getAccounts(function(err,result){
+        coinbase = result[user];    
+        var originalBalance = web3.eth.getBalance(coinbase).toNumber();
+        document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
+        document.getElementById('original').innerHTML = 'original balance: ' + originalBalance + '<br>watching...';
+        web3.eth.filter('latest').watch(function () {
+            var currentBalance = web3.eth.getBalance(coinbase).toNumber();
+            document.getElementById("current").innerText = 'current: ' + currentBalance;
+            document.getElementById("diff").innerText = 'diff: ' + (currentBalance - originalBalance);
+        });
     });
 }
-
 
 $(document).ready(function(){
     $("#watchBalanceBtn").click(function(){
         watchBalance();
+    });
+    $("#signinBtn").click(function(){
+        $("#signinBox").hide();
+        user = $("#usernameInput").val()=="user1" ? 0:1;
+        $("#page-title").html($("#usernameInput").val());
     });
 })
