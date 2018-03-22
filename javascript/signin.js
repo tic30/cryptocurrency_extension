8d853d0d44e5df296726ae6b045bc39762decdf2
Jun <jun.zhang@sv.cmu.edu>
2018-03-21 22:30:54 -0700

    add web3.min.js

--- javascript/signin.js
+++ javascript/signin.js
@@ -1,20 +1,76 @@
 var Web3 = require('web3');
 var web3 = new Web3();
 var user = 0;
-web3.setProvider(new web3.providers.HttpProvider("http://localhost:9545"));
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 function watchBalance() {
-    //var coinbase = web3.eth.coinbase;
+    var coinbase = web3.eth.coinbase;
+    //var abi = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"}];
+    //var abi = [{"constant":false,"inputs":[{"name":"to","type":"address"}],"name":"delegate","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"toVoter","type":"address"}],"name":"giveRightToVote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_numProposals","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":false,"inputs":[{"name":"toProposal","type":"uint8"}],"name":"vote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"winningProposal","outputs":[{"name":"_winningProposal","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"}];
+    var abi = [{"constant":false,"inputs":[{"name":"receiver","type":"address"},{"name":"amount","type":"uint256"}],"name":"sendCoin","outputs":[{"name":"sufficient","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"constant":true,"inputs":[{"name":"addr","type":"address"}],"name":"getBalance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"}]
+    var contractAbi = web3.eth.contract(abi);
+    var contractAddress = '0xd4affae57522a1118776ee05a789d6b718b45cd1';
+    var myContract = contractAbi.at(contractAddress);
+    console.log(myContract);
+
     var coinbase;
     web3.eth.getAccounts(function(err,result){
-        coinbase = result[user];    
-        var originalBalance = web3.eth.getBalance(coinbase).toNumber();
+        coinbase = result[user];
+        console.log(coinbase);   
+        //coinbase = result[2];    
+        var originalBalance = web3.eth.getBalance(coinbase).toString(10);//.toNumber();
+        //var myContractBalance = myContract.getBalance(coinbase);//.then(function(balance){
+            //console.log("Real balance:", myContractBalance.toNumber());
+        //})
+        //myContract.getBalance(coinbase){
+
+        //}
+        
+        var myContractBalance = myContract.getBalance(coinbase, function(error,result){
+            if(error){
+                console.log("Error");
+                throw error;
+            }else{
+                return result;
+            }
+        });
+        //var myContractBalance = myContract.getBalance(coinbase);
+        //console.log("Real balance:", myContractBalance.toNumber());
+        
+        /*var metaCoinBalance = MetaCoin.deployed().then(function(instance) {
+            return instance.getBalance.call(coinbase)
+        }).then(function(outCoinBalance){
+            balance222=outCoinBalance.toNumber(); 
+            return outCoinBalance.toNumber()
+        });*/
+        console.log(originalBalance);
+        console.log(web3.eth.getBalance(coinbase));
+
+        if (user == 0){
+            //Send 0.01 ether to account 2
+            var amount = web3.toWei(0.01, "ether");
+            sendEther(0, 1, amount);
+        }
+
         document.getElementById('coinbase').innerText = 'coinbase: ' + coinbase;
-        document.getElementById('original').innerHTML = 'original balance: ' + originalBalance + '<br>watching...';
+        //document.getElementById('original').innerHTML = 'original balance: ' + metaCoinBalance + '<br>watching...';
         web3.eth.filter('latest').watch(function () {
-            var currentBalance = web3.eth.getBalance(coinbase).toNumber();
+            var currentBalance = web3.eth.getBalance(coinbase)//.toNumber();
             document.getElementById("current").innerText = 'current: ' + currentBalance;
             document.getElementById("diff").innerText = 'diff: ' + (currentBalance - originalBalance);
         });
+
+    });
+}
+
+/*
+*  send amount from sender to receiver. 
+*/
+function sendEther(sender, receiver, amount){
+    web3.eth.getAccounts(function(err,result){
+        var account1 = result[sender];
+        var account2 = result[receiver];
+
+        web3.eth.sendTransaction({from:account1, to:account2, value: amount});
     });
 }
 
